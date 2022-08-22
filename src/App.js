5a2eee45ffd4cdda7f4dcb28a6bfcbfd7d5e1f63
Chimoha31 <mokochii1108@gmail.com>
2022-08-22 12:00:53 -0700

    Done Main title

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Header from './components/Header';
 import MenuBar from './components/MenuBar';
+import Title from './components/Title';
 
 const App = () => {
   return (
     <div>
       <MenuBar />
       <Header />
+      <Title />
     </div>
   )
 }
---
+++ src/components/Title.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Title.scss";
+
+const Title = () => {
+  return (
+    <>
+      <div className="introduce_container mt-5 mb-5">
+        <div className="introduce d-flex flex-column justify-content-center align-items-center">
+          <h2>Hi, I am</h2>
+          <h1>Chiho Maekawa</h1>
+          <p className="text-center">Front-End-Developer</p>
+          <li>
+            <a href="Resume.pdf" download="Resume" className="contact_btn mt-2">
+              <i className="fas fa-download"></i>
+              Resume
+            </a>
+          </li>
+        </div>
+      </div>
+      <div>
+        <hr />
+      </div>
+    </>
+  );
+};
+
+export default Title;
