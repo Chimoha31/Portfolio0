ce0f865d8e4bf38459593f548a763c01f714bdd9
Chimoha31 <mokochii1108@gmail.com>
2022-10-20 23:58:02 -0700

    Changed style

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,6 +6,7 @@ import github from "../gif/github.gif";
 import cellphone from "../gif/cellphone.gif";
 import gmail from "../gif/gmail.gif";
 import ThemeContext from "../context/themeContext";
+import { Alert, Button, FloatingLabel, Form } from "react-bootstrap";
 
 const {
   REACT_APP_MY_SERVICE_ID,
@@ -17,85 +18,113 @@ function Contact() {
   const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [msg, setMsg] = useState("");
+  const [errorMsg, setErrorMsg] = useState("");
+  const [success, setSuccess] = useState("");
   const theme = useContext(ThemeContext);
   const form = useRef();
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs
-      .sendForm(
-        `${REACT_APP_MY_SERVICE_ID}`,
-        `${REACT_APP_MY_TEMPLETE_ID}`,
-        form.current,
-        `${REACT_APP_MY_USER_ID}`
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
+    if (name && email && msg) {
+      setSuccess("Succesfully sent");
+      emailjs
+        .sendForm(
+          `${REACT_APP_MY_SERVICE_ID}`,
+
+          `${REACT_APP_MY_TEMPLETE_ID}`,
+
+          form.current,
+          `${REACT_APP_MY_USER_ID}`
+        )
+        .then(
+          (result) => {
+            console.log(result.text);
+          },
+          (error) => {
+            console.log(error.text);
+          }
+        );
+    } else {
+      setErrorMsg("Please fill in the all blanks");
+    }
     setName("");
     setEmail("");
     setMsg("");
   };
 
-  console.log(name);
+  console.log(errorMsg);
 
   return (
-    <div className="contact_container" style={theme.body}>
-      <h1 style={theme.text}>Reach Me</h1>
-      {/* form */}
-      <div className="contact_form">
-        <div className="form_container">
-          <form ref={form} onSubmit={handleSubmit}>
-            <label>Name</label>
-            <input
-              type="text"
-              name="user_name"
-              autoFocus
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-            />
-            <label>Email</label>
-            <input
-              type="email"
-              name="user_email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <label>Message</label>
-            <textarea
-              name="message"
-              value={msg}
-              onChange={(e) => setMsg(e.target.value)}
-            />
-            <input type="submit" value="Send" className="submit_btn" />
-          </form>
-        </div>
-      </div>
-      <div className="contact_sns">
-        <a href="https://www.linkedin.com/in/chiho1108developer/">
-          <img src={linkedin} alt="linkedin gif" />
-        </a>
-        <a href="https://github.com/Chimoha31">
-          <img src={github} alt="github gif" />
-        </a>
-      </div>
-      <div className="contact_tools">
-        <ul>
-          <li>
-            <img src={cellphone} alt="cellphone gif" />
-            <p style={theme.text}>+1(604)7852569</p>
-          </li>
-          <li>
-            <img src={gmail} alt="gmail gif" />
-            <p style={theme.text}>mokochii1108@gmail.com</p>
-          </li>
-        </ul>
-      </div>
+    <div className="contact d-flex justify-content-center align-items-start mt-5">
+      <Form className="d-flex flex-column form" onSubmit={handleSubmit}>
+        {/* Name */}
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>*Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </Form.Group>
+
+        {/* Email */}
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>*Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+
+        {/* Msg */}
+        <Form.Label>*Comments</Form.Label>
+        <FloatingLabel
+          controlId="floatingTextarea2"
+          label="Message"
+          className="mb-3"
+        >
+          <Form.Control
+            as="textarea"
+            placeholder="Leave a comment here"
+            className="messages"
+            style={{ height: "300px" }}
+            value={msg}
+            onChange={(e) => setMsg(e.target.value)}
+          />
+        </FloatingLabel>
+
+        {errorMsg && !success && (
+          <Alert
+            variant="danger"
+            className="mb-3 p-1 text-center msg"
+            style={{
+              fontFamily:
+                "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+            }}
+          >
+            {errorMsg}
+          </Alert>
+        )}
+        {success && !errorMsg && (
+          <Alert
+            variant="success"
+            className="mb-3 p-1 msg"
+            style={{
+              fontFamily:
+                "'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif",
+            }}
+          >
+            {success}
+          </Alert>
+        )}
+
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
